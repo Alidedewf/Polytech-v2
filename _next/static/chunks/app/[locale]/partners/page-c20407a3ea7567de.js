083ba860ee152d58c4d4d24e4bc0c9908e4c06c6
Alidedewf <Alidedewf@users.noreply.github.com>
2026-09-18 11:30:11 +0000

    deploy: 0301c690ab2ace886e70c4e855f00317d5a209ba 0301c690ab2ace886e70c4e855f00317d5a209ba

---
+++ _next/static/chunks/app/[locale]/partners/page-c20407a3ea7567de.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[916,989],{2377:(e,s,n)=>{Promise.resolve().then(n.bind(n,6558)),Promise.resolve().then(n.t.bind(n,8437,23))}},e=>{e.O(0,[500,558,437,441,794,358],()=>e(e.s=2377)),_N_E=e.O()}]);
